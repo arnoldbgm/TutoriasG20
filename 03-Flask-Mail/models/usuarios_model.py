69166e6b9889ed9289ca55604ebc2721193187f4
arnoldbgm <arnold.gallegosm@gmail.com>
2024-10-12 23:24:06 -0500

    ✨Backend:Flask Mail

--- 03-Flask-Mail/models/usuarios_model.py
+++ 03-Flask-Mail/models/usuarios_model.py
@@ -7,7 +7,3 @@ from sqlalchemy import (Column,
 
 class UsuarioModel(db.Model):
     __tablename__ = 'usuarios'
-    id = Column(Integer, primary_key=True)
-    username = Column(String(150), unique=True, nullable=False)
-    email = Column(String(150), unique=True, nullable=False)
-    password = Column(String(150), nullable=False)
